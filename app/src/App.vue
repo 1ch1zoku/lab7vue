ff196ef2a629439d47c6fb1324258bca153aad34
1ch1zoku <nkhomin666@gmail.com>
2020-12-21 00:18:55 +0200

    Init

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <info></info>
+  
+</template>
+
+<script>
+import Info from './components/Info.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Info
+  }
+}
+</script>
+
+
